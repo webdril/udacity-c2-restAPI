2ec508962ce903954569cc15b5d938fd0f45a243
webdril <jimbahamas03@yahoo.com>
2021-08-16 12:38:29 -0500

    before authentication

---
+++ src/config/config.ts
@@ -0,0 +1,19 @@
+export const config = {
+  "dev": {
+    "username": process.env.POSTGRESS_USERNAME,
+    "password": process.env.POSTGRESS_PASSWORD,
+    "database": process.env.POSTGRESS_DATABASE,
+    "host":     process.env.POSTGRESS_HOST,
+    "dialect": "postgres",
+    "aws_region": "us-east-1",
+    "aws_profile": "udacitylab",
+    "aws_media_bucket": "udagram-eme-dev"
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "udagramemeke",
+    "host": "",
+    "dialect": "postgres"
+  }
+}
